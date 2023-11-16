e751bff0f0c568ec9f7319b5130dbfae2178d0bc
Steffan-S <steffanscholten@hotmail.com>
2023-11-16 17:18:45 +0100

    Added func to get input player & computer. Added getBoard function

--- script.js
+++ script.js
@@ -8,31 +8,50 @@ const gameBoard = (function () {
     // Playboard starts empty
     // Player 1 and Player 2 can fill 1 empty block in turns
     // Add array to gameboard object
-    board = [];
+    let board = [];
     
+    // Creates a black gameboard (erase)
     const blancGameBoard = () => {
-        board = [ , , , , , , , , ,]
-        return board;
+        board = Array(9).fill(null);
     };
 
-    return { blancGameBoard };
+    // Updates board array with players input and their symbol
+    const updateGameBoard = (playerSymbol, input) => {
+        board[input] = playerSymbol;
+        console.log(board);
+    }
+
+    const getBoard = () => {
+        return board;
+    }
+
+    return { board, blancGameBoard, updateGameBoard, getBoard };
 })();
 
 
-gameBoard.blancGameBoard();
-console.log(board);
+
+
+
+
 
 // Create a factory function for the players (player 1 = user, player 2 = computer)
 // When factory is created, create player1 and player2
 function createPlayer(name, symbol) {
-    return { name, symbol };
+    
+    const getInputPlayer = function () {
+        const input = window.prompt("Your turn");
+        return input;
+    };
+    
+    return { name, symbol, getInputPlayer };
 }
 
 const playerUser = createPlayer('Steffan', 'X');
-console.log(playerUser);
+// console.log(playerUser);
+// console.log(playerUser.getInputPlayer());
 
 const playerComputer = createPlayer('Robotucus', 'O');
-console.log(playerComputer);
+// console.log(playerComputer);
 
 
 // create and add functionality to gamecontroller object to check gamestatus
@@ -40,7 +59,24 @@ console.log(playerComputer);
 // If not check if all 9 blocks are full = draw
 // If both not true continue game
 // Do this after every move from player 1 or 2
-// const gameController = 
+const gameController = (function (gameboard, player1, player2) {
+    const gameEndCheck = function (gameBoard) {
+        let filledBlocks = 0;
+
+        for (const block of gameBoard){
+            console.log(block);
+            if (block.length !== 0) {
+                filledBlocks++;
+            };
+        };
+
+        if (filledBlocks === 2){
+            console.log('DRAW, END GAME');
+        };
+    };
+
+    return {gameEndCheck};
+})();
 
 
 
@@ -52,3 +88,20 @@ console.log(playerComputer);
 
 
 
+
+
+
+// tests
+
+
+gameBoard.blancGameBoard();
+
+gameBoard.updateGameBoard(playerUser.symbol, playerUser.getInputPlayer());
+gameBoard.updateGameBoard(playerComputer.symbol, playerComputer.getInputPlayer());
+console.log(gameBoard.getBoard());
+
+gameController.gameEndCheck(gameBoard.board);
+
+
+
+// console.log(board);
